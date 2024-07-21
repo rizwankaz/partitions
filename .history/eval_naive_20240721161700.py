e1dba5f4ea03ca398a5568077c1651339d1c365c
Rizwan Kazi <rsk2176@columbia.edu>
2024-07-21 16:18:43 -0400

    evaluation scripts for naive algorithm

---
+++ .history/eval_naive_20240721161700.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+n_values = []
+runtime_values = []
+
+with open("runtime_data.txt", "r") as file:
+    next(file)
+    for line in file:
+        parts = line.strip().split('\t')
+        n = int(parts[0])
+        runtime = float(parts[2])
+        n_values.append(n)
+        runtime_values.append(runtime)
+
+plt.figure(figsize=(10, 6))
+plt.plot(n_values, runtime_values, marker='o', linestyle='-', color='b', linewidth=0.25, markersize=1)
+plt.xlabel('n')
+plt.ylabel('runtime(n)')
+plt.title('Runtime of partition_naive(n)')
+plt.grid(True)
+plt.savefig("runtime_plot.png")
+plt.show()
---
+++ .history/partition_naive_plot_20240721161136.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+n_values = []
+partition_values = []
+
+with open("runtime_data.txt", "r") as file:
+    next(file)
+    for line in file:
+        parts = line.strip().split('\t')
+        n = int(parts[0])
+        partition = int(parts[1])
+        n_values.append(n)
+        partition_values.append(partition)
+
+plt.figure(figsize=(10, 6))
+plt.plot(n_values, partition_values, marker='o', linestyle='-', color='b', linewidth=0.5)
+plt.xlabel('n')
+plt.ylabel('partition(n)')
+plt.yscale('log')
+plt.grid(True)
+plt.savefig("partition_plot.png")
+plt.show()
